a6448467e37f5a2c552c5b5d328c5312db64d1c7
Dmitry Jemerov <yole@jetbrains.com>
2020-12-11 18:07:35 +0100

    Day 8 step 2

--- day08/day08.go
+++ day08/day08.go
@@ -21,19 +21,62 @@ type instruction struct {
 
 func main() {
 	fmt.Println(solveDay8Step1("day08input.txt"))
+	fmt.Println(solveDay8Step2("day08input.txt"))
 }
 
 func solveDay8Step1(input string) int {
+	program := loadProgram(input)
+	_, acc := checkTerminates(program, 0, 0)
+	return acc
+}
+
+func solveDay8Step2(input string) int {
+	program := loadProgram(input)
+	accSequence := make([]int, len(program))
+	ipSequence := make([]int, len(program))
+	visited := make([]bool, len(program))
+	ip := 0
+	acc := 0
+	step := 0
+	for {
+		if visited[ip] {
+			break
+		}
+		visited[ip] = true
+		acc, ip = executeInstruction(program, ip, acc)
+		accSequence[step] = acc
+		ipSequence[step] = ip
+		step++
+	}
+
+	for i := step - 1; i >= 0; i-- {
+		if program[ipSequence[i]].opcode != ACC {
+			terminates, acc := checkTerminates(patchProgram(program, ipSequence[i]), accSequence[i], ipSequence[i])
+			if terminates {
+				return acc
+			}
+		}
+	}
+
+	return -1
+}
+
+func loadProgram(input string) []instruction {
 	program := make([]instruction, 0)
 	shared.ProcessInputLines(input, func(line string) {
 		program = append(program, parseInstruction(line))
 	})
+	return program
+}
+
+func checkTerminates(program []instruction, acc int, ip int) (bool, int) {
 	visited := make([]bool, len(program))
-	acc := 0
-	ip := 0
 	for {
+		if ip == len(program) {
+			return true, acc
+		}
 		if visited[ip] {
-			return acc
+			return false, acc
 		}
 		visited[ip] = true
 		acc, ip = executeInstruction(program, ip, acc)
@@ -81,4 +124,15 @@ func parseInstruction(line string) instruction {
 		opcode:  opcode,
 		operand: operand,
 	}
+}
+
+func patchProgram(program []instruction, offset int) []instruction {
+	patchedProgram := make([]instruction, len(program))
+	copy(patchedProgram, program)
+	if patchedProgram[offset].opcode == NOP {
+		patchedProgram[offset].opcode = JMP
+	} else {
+		patchedProgram[offset].opcode = NOP
+	}
+	return patchedProgram
 }
--- day08/day08_test.go
+++ day08/day08_test.go
@@ -8,3 +8,10 @@ func TestSolveDay8Step1(t *testing.T) {
 		t.Errorf("Unexpected result %d", result)
 	}
 }
+
+func TestSolveDay8Step2(t *testing.T) {
+	result := solveDay8Step2("day08testinput.txt")
+	if result != 8 {
+		t.Errorf("Unexpected result %d", result)
+	}
+}
